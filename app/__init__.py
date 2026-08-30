eac4a53bc8205826ec0462647666d5d270e1afeb
khaybet <34009776+khaybet@users.noreply.github.com>
2026-08-30 22:21:04 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 
 def create_app():
-    app = Flask(__name__)
-
-    from .routes import main
+    app = Flask(
+    __name__,
+    template_folder="app/templates",
+    static_folder="app/static"
+)
+   from .routes import main
     app.register_blueprint(main)
 
     return app
