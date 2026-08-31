e7972f2b87f95763104fbbf4c38e5b681f702348
khaybet <34009776+khaybet@users.noreply.github.com>
2026-08-31 09:46:03 +0100

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -1,5 +1,19 @@
-ydl_opts = {
-    "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best",
-    "merge_output_format": "mp4",
-    "outtmpl": "/downloads/%(title)s.%(ext)s",
-}
+import yt_dlp
+import os
+
+def download_video(url):
+    download_dir = "/app/downloads"
+    os.makedirs(download_dir, exist_ok=True)
+
+    output = f"{download_dir}/%(title)s.%(ext)s"
+
+    ydl_opts = {
+        "outtmpl": output,
+        "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4"
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=True)
+        filename = ydl.prepare_filename(info)
+
+    return os.path.abspath(filename)
